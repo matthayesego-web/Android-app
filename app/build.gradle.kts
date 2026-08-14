2631a32d0e9d12e2a874790b779e033f838b4512
matthayesego-web <matt.hayesego@gmail.com>
2026-08-14 11:46:21 -0400

    Bump Android version to v0.4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.matthayesego.duckforcetoolkit"
         minSdk = 24
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.4.2"
+        versionCode = 8
+        versionName = "0.4.3"
     }
 
     buildTypes {
