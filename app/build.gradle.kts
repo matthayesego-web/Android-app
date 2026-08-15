b0aa851b75303a65878938acab8af042b4298c43
matthayesego-web <matt.hayesego@gmail.com>
2026-08-15 00:03:37 -0400

    Prepare v0.7.0 version metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.matthayesego.duckforcetoolkit"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "0.6.0"
+        versionCode = 13
+        versionName = "0.7.0"
         manifestPlaceholders["appLabel"] = "Duck Force Companion"
     }
 
