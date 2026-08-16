fb04aeaea9308270401537826701b7cfd025f0ef
matthayesego-web <matt.hayesego@gmail.com>
2026-08-15 21:28:14 -0400

    Bump beta to v0.9.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.matthayesego.duckforcetoolkit"
         minSdk = 24
         targetSdk = 36
-        versionCode = 16
-        versionName = "0.9.1"
+        versionCode = 17
+        versionName = "0.9.2"
         manifestPlaceholders["appLabel"] = "Duck Force Companion"
     }
 
