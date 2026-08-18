3cab20cba41375ceeeffcf7ce085f04ee68853f7
matthayesego-web <matt.hayesego@gmail.com>
2026-08-17 23:05:20 -0400

    Keep legal acceptance on canonical command shell

--- app/src/main/java/com/matthayesego/duckforcetoolkit/LegalActivity.java
+++ app/src/main/java/com/matthayesego/duckforcetoolkit/LegalActivity.java
@@ -95,7 +95,7 @@ public class LegalActivity extends Activity {
                 }
                 LegalAcceptanceStore.acceptCurrent(this);
                 PushNotifications.initialize(this);
-                Intent i=new Intent(this,TornFcaCurrentActivity.class);
+                Intent i=TornFcaCommandRuntime.homeIntent(this,"Home");
                 i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
                 startActivity(i);
                 finish();
