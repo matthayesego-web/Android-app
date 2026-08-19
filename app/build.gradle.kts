fce5161d832b6be8eec84564a0f40f63c3bd60b1
matthayesego-web <matt.hayesego@gmail.com>
2026-08-19 09:37:34 -0400

    Bump Beta stability pass to v0.10.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.matthayesego.duckforcetoolkit"
         minSdk = 24
         targetSdk = 36
-        versionCode = 66
-        versionName = "0.10.11"
+        versionCode = 67
+        versionName = "0.10.12"
         manifestPlaceholders["appLabel"] = "TornFCA"
 
         val factionBackend = localOrEnv("TORNFCA_FACTION_BACKEND_URL", "https://script.google.com/macros/s/AKfycbzR8zjFVYaDoIZaxFdJV4yBaM4mLSv-iqZL8146HCzq6vRzJgeYr1Y0TKkPGrdASjQj/exec")
